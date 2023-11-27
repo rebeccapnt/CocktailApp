0605331d45b7ceddaf6df0f18b6285df5433e16c
LOHEZIC VICTOR <vlohezic@enseirb-matmeca.fr>
2023-11-27 21:11:20 +0100

    [feat] make fetcher generic

--- app/src/main/java/com/example/cocktailtemplate/core/service/Fetcher.kt
+++ app/src/main/java/com/example/cocktailtemplate/core/service/Fetcher.kt
@@ -1,16 +1,20 @@
 package com.example.cocktailtemplate.core.service
 
 import android.util.Log
+import com.example.cocktailtemplate.core.model.ApiResponse
 import com.example.cocktailtemplate.core.model.Category
 import com.example.cocktailtemplate.core.model.Cocktail
 import com.example.cocktailtemplate.core.model.CocktailList
 import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
 import okhttp3.Call
 import okhttp3.Callback
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
 import okio.IOException
+import java.lang.reflect.Type
 import java.net.URL
 import java.text.CollationKey
 
@@ -18,11 +22,14 @@ class Fetcher {
 
     companion object {
 
-        private val client = OkHttpClient()
-        private const val rootUrl = "https://www.thecocktaildb.com/api/json/v1/1/"
-        private const val detailEndPoint = "lookup.php?i="
-        fun fetchDetail(idCocktail: Int, success: (CocktailList) -> Unit, failure: (Error) -> Unit) {
-            val url = URL(rootUrl + detailEndPoint + idCocktail.toString())
+        public val client = OkHttpClient()
+        public const val rootUrl = "https://www.thecocktaildb.com/api/json/v1/1/"
+        inline fun <reified T> fetchDetail(
+            detailEndPoint: String,
+            crossinline success: (ApiResponse<T>) -> Unit,
+            crossinline failure: (Error) -> Unit
+        ) {
+            val url = URL(rootUrl + detailEndPoint)
             val request = Request.Builder().url(url).build()
 
             client.newCall(request).enqueue(object : Callback {
@@ -33,12 +40,12 @@ class Fetcher {
 
                 override fun onResponse(call: Call, response: Response) {
                     Log.i("OKHTTP", "OnSuccess")
-                    val gson = Gson()
-                    val detailCocktailResponse = gson.fromJson(
-                        response.body?.string(),
-                        CocktailList::class.java
-                    )
-                    success(detailCocktailResponse)
+                    val gson = GsonBuilder().create()
+
+                    val collectionType: Type = object : TypeToken<ApiResponse<T>?>() {}.type
+                    val apiResponse: ApiResponse<T> = gson.fromJson(response.body?.string(), collectionType)
+
+                    success(apiResponse)
                 }
             })
         }
